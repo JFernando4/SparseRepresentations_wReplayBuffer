ecb6a303e28e91a351d6a27712766e1a8b37a066
jfernando <jfhernan@ualberta.ca>
2019-02-23 14:11:29 -0700

    added check if directory exists

--- Slurm_Scheduling_Files/ab_experience_replay.sh
+++ Slurm_Scheduling_Files/ab_experience_replay.sh
@@ -5,10 +5,16 @@
 #SBATCH --time=1:30:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=1000M
-#SBATCH --job-name=mc_er
-#SBATCH --output=./outputs/mc_er-%A_%a.out
+#SBATCH --job-name=ab_er
+#SBATCH --output=./outputs/ab_er-%A_%a.out
 
 source ./bin/activate
 export PYTHONPATH=.
 python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env acrobot -run_number $SLURM_ARRAY_TASK_ID
 deactivate
+# Parameter Sweep:
+# learning rate = {0.01, 0.004, 0.001, 0.00025, 0.0000625}
+# buffer size = {10k, 20k, 40k}
+# target network update frequency = {10, 50, 100, 200, 400}
+    # We tested a frequency of 1 but in most runs learning was very brittle. In the few runs where the network
+    # actually managed to learn, it often forgot what it had learned after a few episodes.
