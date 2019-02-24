a69efad2999467f61ed92ee5e286c6834e0b4afb
jfernando <jfhernan@ualberta.ca>
2019-02-24 15:19:53 -0700

    added DistributionalReg_Experiment and its slurm scheduling file

--- DistritbutionalReg_Experiment.py
+++ DistritbutionalReg_Experiment.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import torch
 import pickle
+import time
 
 from Experiment_Engine.util import check_attribute_else_default, Config     # utilities
 from Experiment_Engine import Acrobot, MountainCar, PuddleWorld             # environments
@@ -14,27 +15,22 @@ NUMBER_OF_EPISODES = 500
 class Experiment:
 
     def __init__(self, experiment_parameters, run_results_dir):
-        """" Parameters of the experiment given by the user """
         self.run_results_dir = run_results_dir
+        self.learning_rate = check_attribute_else_default(exp_parameters, 'lr', 0.001)
+        self.beta = check_attribute_else_default(experiment_parameters, 'beta', 0.1)
+        self.reg_factor = check_attribute_else_default(experiment_parameters, 'reg_factor', 0.1)
+        self.use_gamma = check_attribute_else_default(experiment_parameters, 'use_gamma', False)
         self.environment_name = check_attribute_else_default(experiment_parameters, 'env', 'mountain_car',
                                                              choices=['mountain_car', 'acrobot', 'puddle_world'])
-        self.learning_rate = check_attribute_else_default(experiment_parameters, 'lr', 0.001,
-                                                          choices=[0.01, 0.004, 0.001, 0.00025, 0.0000625])
-        self.reg_factor = check_attribute_else_default(experiment_parameters, 'reg_factor', 0.01,
-                                                       choices=[0.1, 0.01, 0.001])
-        self.beta = check_attribute_else_default(experiment_parameters, 'beta', 0.1,
-                                                 choices=[0.1, 0.2, 0.5])
-        self.ma_alpha = check_attribute_else_default(experiment_parameters, 'ma_alpha', 0.1,
-                                                     choices=[0.1, 0.01])
-        self.use_gamma = check_attribute_else_default(experiment_parameters, 'use_gamma', False)
         self.verbose = experiment_parameters.verbose
 
-        """ Experiment set up """
         environment_dictionary = {
-            'mountain_car': {'class': MountainCar, 'state_dims': 2, 'num_actions': 3},
+            # the buffer size and target network update frequency parameters were chosen by optimizing the DQN agent
+            'mountain_car': {'class': MountainCar, 'state_dims': 2, 'num_actions': 3, 'buffer_size': 20000, 'freq': 10},
             'acrobot': {'class': Acrobot, 'state_dims': 4, 'num_actions': 3},
             'puddle_world': {'class': PuddleWorld, 'state_dims': 2, 'num_actions': 4}
                                   }
+
         self.config = Config()
         self.config.store_summary = True
         self.summary = {}
@@ -49,13 +45,15 @@ class Experiment:
         self.config.gamma = 1.0
         self.config.epsilon = 0.1
         self.config.optim = "adam"
+        self.config.batch_size = 32
+        self.config.training_step_count = 0
+        self.config.buffer_size = environment_dictionary[self.environment_name]['buffer_size']
+        self.config.tnet_update_freq = environment_dictionary[self.environment_name]['freq']
+        self.config.gates = 'relu-relu'
+
         self.config.lr = self.learning_rate
-        self.config.reg_factor = self.reg_factor
         self.config.beta = self.beta
-        if self.use_gamma:
-            self.config.ma_alpha = 0.0
-        else:
-            self.config.ma_alpha = self.ma_alpha
+        self.config.reg_factor = self.reg_factor
         self.config.use_gamma = self.use_gamma
 
         self.env = environment_dictionary[self.environment_name]['class'](config=self.config, summary=self.summary)
@@ -90,59 +88,46 @@ class Experiment:
 
 
 if __name__ == '__main__':
+    time.sleep(np.random.rand())
     """ Experiment Parameters """
     parser = argparse.ArgumentParser()
     parser.add_argument('-run_number', action='store', default=1, type=int)
     parser.add_argument('-env', action='store', default='mountain_car', type=str,
                         choices=['mountain_car', 'acrobot', 'puddle_world'])
+    parser.add_argument('-beta', action='store', default=0.1, type=np.float64, choices=[0.1, 0.2, 0.5])
+    parser.add_argument('-reg_factor', action='store', default=0.1, type=np.float64, choices=[0.1, 0.01, 0.001])
     parser.add_argument('-lr', action='store', default=0.001, type=np.float64,
                         choices=[0.01, 0.004, 0.001, 0.00025, 0.0000625])
-    parser.add_argument('-reg_factor', action='store', default=0.1, type=float,
-                        choices=[0.1, 0.01, 0.001])
-    parser.add_argument('-beta', action='store', default=0.1, type=np.float64,
-                        choices=[0.1, 0.2, 0.5])
-    parser.add_argument('-ma_alpha', action='store', default=0.1, type=np.float64,
-                        choices=[0.1, 0.01])
     parser.add_argument('-use_gamma', action='store_true')
     parser.add_argument('-verbose', action='store_true')
     exp_parameters = parser.parse_args()
 
-    """ This part creates the directory: working_dir/Results/environment/method/parameter_combination/agent_number"""
-    # General results directory
+    """ General results directory """
     results_parent_directory = os.path.join(os.getcwd(), 'Results')
     if not os.path.exists(results_parent_directory):
         os.makedirs(results_parent_directory)
+    """ Directory specific to the environment and the method """
     if exp_parameters.use_gamma:
-        # Directory specific to the environment and the method
         environment_result_directory = os.path.join(results_parent_directory, exp_parameters.env,
-                                                    'distributional_regularizers_gamma_distribution')
-        if not os.path.exists(environment_result_directory):
-            os.makedirs(environment_result_directory)
-        # Directory specific to the parameters
-        parameters_name = 'LearningRate' + str(exp_parameters.lr) \
-                          + '_RegFactor' + str(exp_parameters.reg_factor) \
-                          + '_Beta' + str(exp_parameters.beta)
-        parameters_result_directory = os.path.join(environment_result_directory, parameters_name)
-        if not os.path.exists(parameters_result_directory):
-            os.makedirs(parameters_result_directory)
+                                                    'DistributionalRegularizers_Gamma')
     else:
-        # Directory specific to the environment and the method
         environment_result_directory = os.path.join(results_parent_directory, exp_parameters.env,
-                                                    'distributional_regularizers')
-        if not os.path.exists(environment_result_directory):
-            os.makedirs(environment_result_directory)
-        # Directory specific to the parameters
-        parameters_name = 'LearningRate' + str(exp_parameters.lr) \
-                          + '_RegFactor' + str(exp_parameters.reg_factor) \
-                          + '_Beta' + str(exp_parameters.beta) \
-                          + '_MovingAvgAlpha' + str(exp_parameters.ma_alpha)
-        parameters_result_directory = os.path.join(environment_result_directory, parameters_name)
-        if not os.path.exists(parameters_result_directory):
-            os.makedirs(parameters_result_directory)
-    # Directory specific to the run
+                                                    'DistributionalRegularizers_Beta')
+    if not os.path.exists(environment_result_directory):
+        os.makedirs(environment_result_directory)
+    """ Directory specific to the parameters"""
+    parameters_name = 'LearningRate' + str(exp_parameters.lr) \
+                      + '_Beta' + str(exp_parameters.beta) \
+                      + "_RegFactor" + str(exp_parameters.reg_factor)
+    parameters_result_directory = os.path.join(environment_result_directory, parameters_name)
+    if not os.path.exists(parameters_result_directory):
+        os.makedirs(parameters_result_directory)
+    """ Directory specific to the run """
     agent_id = 'agent_' + str(exp_parameters.run_number)
     run_results_directory = os.path.join(parameters_result_directory, agent_id)
-    os.makedirs(run_results_directory)
+    print("The agent results directory is:", run_results_directory)
+    if not os.path.exists(run_results_directory):
+        os.makedirs(run_results_directory)
 
     """ Setting up and running the experiment """
     experiment = Experiment(experiment_parameters=exp_parameters, run_results_dir=run_results_directory)
@@ -152,5 +137,3 @@ if __name__ == '__main__':
 # learning rate = {0.01, 0.004, 0.001, 0.00025, 0.0000625}
 # reg_factor = {0.1, 0.01, 0.001}
 # beta = {0.1, 0.2, 0.5}
-# ma_alpha = {0.1, 0.01}
-    # ma_alpha is not necessary when using a gamma distribution
