2aee52e91bfb01e36927ce6fede0b77bad9036ed
jfernando <jfhernan@ualberta.ca>
2019-02-25 20:22:08 -0700

    compute_activation_map now returns the activation maps of both layers

--- Experiment_Engine/network_evaluation.py
+++ Experiment_Engine/network_evaluation.py
@@ -4,31 +4,31 @@ import numpy as np
 from .networks import TwoLayerFullyConnected
 
 
-def compute_activation_map(network, granularity=100, layer=1):
+def compute_activation_map(network, granularity=100):
     """
     :param network: an instance of the class TwoLayerFullyConnected
     :param granularity: how fine should it be the partition on each direction
-    :param layer: layer used to compute the activation map
     :param sample_size: size of the sample
     :return: random sample of activation maps of non-dead neurons
     """
-    assert layer in [1, 2]
     assert isinstance(network, TwoLayerFullyConnected)
     assert network.fc1.in_features == 2                                     # The function admits only 2D state spaces
 
-    layer_activation = network.first_layer_neurons if layer == 1 else network.second_layer_neurons
-    num_neurons = network.fc1.out_features if layer == 1 else network.fc2.out_features
-
+    layer1_num_neurons = 32
+    layer2_num_neurons = 256
     partition_size = 2 / (granularity - 1)
     state_partition = np.arange(-1, 1 + partition_size, partition_size, dtype=np.float64)
-    activation_maps = np.zeros((num_neurons, granularity, granularity), dtype=np.float64)
+    activation_maps_layer1 = np.zeros((layer1_num_neurons, granularity, granularity), dtype=np.float64)
+    activation_maps_layer2 = np.zeros((layer2_num_neurons, granularity, granularity), dtype=np.float64)
 
     for i in range(granularity):
         for j in range(granularity):
             temp_state = np.array((state_partition[i], state_partition[j]), dtype=np.float64)
-            activation_maps[:, i, j] = layer_activation(temp_state).detach().numpy()
+            x1, x2, = network.forward(temp_state, return_activations=True)
+            activation_maps_layer1[:, i, j] = x1.detach().numpy()
+            activation_maps_layer2[:, i, j] = x2.detach().numpy()
 
-    return eliminate_dead_neuron_maps(activation_maps)
+    return eliminate_dead_neuron_maps(activation_maps_layer1), eliminate_dead_neuron_maps(activation_maps_layer2)
 
 
 def eliminate_dead_neuron_maps(activation_maps):
