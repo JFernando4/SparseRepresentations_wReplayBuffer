ba7ee8ea109b2f4447ef615c6540a8398d37da5d
jfernando <jfhernan@ualberta.ca>
2019-04-08 18:41:19 -0600

    updated the best parameters for dist reg gamma buffer size = 1k, 5k, 80k

--- BestParameters_Experiment.py
+++ BestParameters_Experiment.py
@@ -37,10 +37,10 @@ BEST_PARAMETERS_DICTIONARY = {
     'DistributionalRegularizers_Gamma': {
         # Buffer Size
         100: {'Freq': 400, 'LearningRate': 0.004, 'Beta': 0.2, 'RegFactor': 0.1},           # Wrong
-        1000: {'Freq': 10, 'LearningRate': 0.004, 'Beta': 0.2, 'RegFactor': 0.1},           # Wrong
-        5000: {'Freq': 10, 'LearningRate': 0.004, 'Beta': 0.5, 'RegFactor': 0.1},           # Wrong
+        1000: {'Freq': 10, 'LearningRate': 0.004, 'Beta': 0.2, 'RegFactor': 0.1},
+        5000: {'Freq': 10, 'LearningRate': 0.004, 'Beta': 0.2, 'RegFactor': 0.1},
         20000: {'Freq': 10, 'LearningRate': 0.001, 'Beta': 0.5, 'RegFactor': 0.1},
-        80000: {'Freq': 10, 'LearningRate': 0.001, 'Beta': 0.2, 'RegFactor': 0.001},        # Wrong
+        80000: {'Freq': 10, 'LearningRate': 0.001, 'Beta': 0.2, 'RegFactor': 0.1},
         'ParameterNames': ['BufferSize', 'Freq', 'LearningRate', 'Beta', 'RegFactor']
     },
 
