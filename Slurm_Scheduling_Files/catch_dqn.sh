2b91add15a54819f7646c11ca49bec5676c36f35
jfernando <jfhernan@ualberta.ca>
2019-04-23 04:41:22 -0600

    changed to 2 runs per job and 4 hours total

--- Slurm_Scheduling_Files/catch_dqn.sh
+++ Slurm_Scheduling_Files/catch_dqn.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=ALL
 #SBATCH --array=1-10
-#SBATCH --time=5:00:00
+#SBATCH --time=4:00:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=500M
 #SBATCH --job-name=catch_dqn
@@ -10,11 +10,11 @@
 
 source ./bin/activate
 export PYTHONPATH=.
-RUN1=$(($SLURM_ARRAY_TASK_ID*3 - 2))
-RUN2=$(($SLURM_ARRAY_TASK_ID*3 - 1))
-RUN3=$(($SLURM_ARRAY_TASK_ID*3))
+#RUN1=$(($SLURM_ARRAY_TASK_ID*3 - 2))
+RUN2=$(($SLURM_ARRAY_TASK_ID*2 - 1))
+RUN3=$(($SLURM_ARRAY_TASK_ID*2))
 
-python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN1
+#python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN1
 python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN2
 python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN3
 deactivate
