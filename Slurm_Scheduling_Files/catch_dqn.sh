82978af5f80436cced2865b20f15f949a2667eba
jfernando <jfhernan@ualberta.ca>
2019-04-23 10:05:58 -0600

    increased the number of array jobs from 10 to 15

--- Slurm_Scheduling_Files/catch_dqn.sh
+++ Slurm_Scheduling_Files/catch_dqn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-10
+#SBATCH --array=1-15
 #SBATCH --time=4:00:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=500M
@@ -10,11 +10,9 @@
 
 source ./bin/activate
 export PYTHONPATH=.
-#RUN1=$(($SLURM_ARRAY_TASK_ID*3 - 2))
-RUN2=$(($SLURM_ARRAY_TASK_ID*2 - 1))
-RUN3=$(($SLURM_ARRAY_TASK_ID*2))
+RUN1=$(($SLURM_ARRAY_TASK_ID*2 - 1))
+RUN2=$(($SLURM_ARRAY_TASK_ID*2))
 
-#python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN1
+python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN1
 python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN2
-python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN3
 deactivate
