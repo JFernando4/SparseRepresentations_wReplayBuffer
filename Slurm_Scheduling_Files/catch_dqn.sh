988fa4422649a1655e372accd0436a2f6a28bae9
jfernando <jfhernan@ualberta.ca>
2019-04-23 18:42:16 -0600

    changed from 2 runs to 1 run in 3 hours

--- Slurm_Scheduling_Files/catch_dqn.sh
+++ Slurm_Scheduling_Files/catch_dqn.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=ALL
 #SBATCH --array=1-15
-#SBATCH --time=4:00:00
+#SBATCH --time=3:00:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=500M
 #SBATCH --job-name=catch_dqn
@@ -10,9 +10,7 @@
 
 source ./bin/activate
 export PYTHONPATH=.
-RUN1=$(($SLURM_ARRAY_TASK_ID*2 - 1))
-RUN2=$(($SLURM_ARRAY_TASK_ID*2))
+RUN2=$(($SLURM_ARRAY_TASK_ID))
 
-python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN1
 python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env catcher -run_number $RUN2
 deactivate
