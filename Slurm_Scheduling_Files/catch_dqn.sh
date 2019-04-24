8394420b2609dbe1afabe960258014b4787c13ce
jfernando <jfhernan@ualberta.ca>
2019-04-23 18:43:11 -0600

    now 30 runs instead of 15

--- Slurm_Scheduling_Files/catch_dqn.sh
+++ Slurm_Scheduling_Files/catch_dqn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-15
+#SBATCH --array=1-30
 #SBATCH --time=3:00:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=500M
