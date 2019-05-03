51cebc7fd6ef8e5cf069c831f2391417c9153962
jfernando <jfhernan@ualberta.ca>
2019-05-03 01:33:05 -0600

    corrected typo

--- Slurm_Scheduling_Files/catch_l1w.sh
+++ Slurm_Scheduling_Files/catch_l1w.sh
@@ -15,9 +15,9 @@ RUN2=$(($SLURM_ARRAY_TASK_ID*3 - 1))
 RUN3=$(($SLURM_ARRAY_TASK_ID*3))
 
 python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
--beta $BETA -reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN1
+-reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN1
 python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
--beta $BETA -reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN2
+-reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN2
 python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
--beta $BETA -reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN3
+-reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN3
 deactivate
