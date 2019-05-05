de563c7323d0975b423f921baed9a0af991a4a52
jfernando <jfhernan@ualberta.ca>
2019-05-05 12:17:44 -0600

    now does 5 runs per job in 8:20 hours

--- Slurm_Scheduling_Files/catch_drg.sh
+++ Slurm_Scheduling_Files/catch_drg.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=ALL
 #SBATCH --array=1-10
-#SBATCH --time=5:00:00
+#SBATCH --time=8:20:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=500M
 #SBATCH --job-name=catch_drg
@@ -10,9 +10,11 @@
 
 source ./bin/activate
 export PYTHONPATH=.
-RUN1=$(($SLURM_ARRAY_TASK_ID*3 - 2))
-RUN2=$(($SLURM_ARRAY_TASK_ID*3 - 1))
-RUN3=$(($SLURM_ARRAY_TASK_ID*3))
+RUN1=$(($SLURM_ARRAY_TASK_ID*5 - 4))
+RUN2=$(($SLURM_ARRAY_TASK_ID*5 - 3))
+RUN3=$(($SLURM_ARRAY_TASK_ID*5 - 2))
+RUN4=$(($SLURM_ARRAY_TASK_ID*5 - 1))
+RUN5=$(($SLURM_ARRAY_TASK_ID*5))
 
 python3 ./DistritbutionalReg_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
 -beta $BETA -reg_factor $RF -use_gamma -env catcher -run_number $RUN1
@@ -20,4 +22,8 @@ python3 ./DistritbutionalReg_Experiment.py -buffer_size $BUFFER -tnet_update_fre
 -beta $BETA -reg_factor $RF -use_gamma -env catcher -run_number $RUN2
 python3 ./DistritbutionalReg_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
 -beta $BETA -reg_factor $RF -use_gamma -env catcher -run_number $RUN3
+python3 ./DistritbutionalReg_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
+-beta $BETA -reg_factor $RF -use_gamma -env catcher -run_number $RUN4
+python3 ./DistritbutionalReg_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
+-beta $BETA -reg_factor $RF -use_gamma -env catcher -run_number $RUN5
 deactivate
