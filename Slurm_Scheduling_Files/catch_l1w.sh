236c8674a3d8249646bb164606c5413495c548c1
jfernando <jfhernan@ualberta.ca>
2019-05-07 16:56:14 -0600

    fixed bug were the first summary store was always zero

--- Slurm_Scheduling_Files/catch_l1w.sh
+++ Slurm_Scheduling_Files/catch_l1w.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-10
-#SBATCH --time=5:00:00
+#SBATCH --array=1-6
+#SBATCH --time=10:00:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=500M
 #SBATCH --job-name=catch_l1w
@@ -10,9 +10,11 @@
 
 source ./bin/activate
 export PYTHONPATH=.
-RUN1=$(($SLURM_ARRAY_TASK_ID*3 - 2))
-RUN2=$(($SLURM_ARRAY_TASK_ID*3 - 1))
-RUN3=$(($SLURM_ARRAY_TASK_ID*3))
+RUN1=$(($SLURM_ARRAY_TASK_ID*5 - 4))
+RUN2=$(($SLURM_ARRAY_TASK_ID*5 - 3))
+RUN3=$(($SLURM_ARRAY_TASK_ID*5 - 2))
+RUN4=$(($SLURM_ARRAY_TASK_ID*5 - 1))
+RUN5=$(($SLURM_ARRAY_TASK_ID*5))
 
 python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
 -reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN1
@@ -20,4 +22,8 @@ python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $F
 -reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN2
 python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
 -reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN3
+python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
+-reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN4
+python3 ./Regularization_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR \
+-reg_factor $RF -env catcher -weights_reg -l1_reg -run_number $RUN5
 deactivate
