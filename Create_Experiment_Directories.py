d0091ce6e76b4f4201f4caaabdae43046855fe98
jfernando <jfhernan@ualberta.ca>
2019-05-10 12:36:42 -0600

    added best parameters for l2a

--- Create_Experiment_Directories.py
+++ Create_Experiment_Directories.py
@@ -277,11 +277,11 @@ if __name__ == '__main__':
 
                 'L2_Regularization_OnActivations': {
                     # Buffer Size
-                    100: {'Freq': 10, 'LearningRate': 0, 'RegFactor': 0},
-                    1000: {'Freq': 50, 'LearningRate': 0, 'RegFactor': 0},
-                    5000: {'Freq': 200, 'LearningRate': 0, 'RegFactor': 0},
-                    20000: {'Freq': 200, 'LearningRate': 0, 'RegFactor': 0},
-                    80000: {'Freq': 400, 'LearningRate': 0, 'RegFactor': 0},
+                    100: {'Freq': 10, 'LearningRate': 0.0000625, 'RegFactor': 0.0005},
+                    1000: {'Freq': 50, 'LearningRate': 0.0000625, 'RegFactor': 0.01},
+                    5000: {'Freq': 200, 'LearningRate': 0.0000625, 'RegFactor': 0.001},
+                    20000: {'Freq': 200, 'LearningRate': 0.0000625, 'RegFactor': 0.001},
+                    80000: {'Freq': 400, 'LearningRate': 0.00025, 'RegFactor': 0.0001},
                     'ParameterNames': ['BufferSize', 'Freq', 'LearningRate', 'RegFactor']
                 },
 
