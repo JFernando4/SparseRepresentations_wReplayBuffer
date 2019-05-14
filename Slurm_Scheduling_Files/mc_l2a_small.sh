9b8abeb92b54633a726395540766dceee09a931e
jfernando <jfhernan@ualberta.ca>
2019-05-14 08:47:13 -0600

    fixed typo: missing space between $FREQ and -lr

--- Slurm_Scheduling_Files/mc_l2a_small.sh
+++ Slurm_Scheduling_Files/mc_l2a_small.sh
@@ -18,15 +18,15 @@ RUN4=$(($SLURM_ARRAY_TASK_ID*5 - 1))
 RUN5=$(($SLURM_ARRAY_TASK_ID*5))
 
 python3 ./Regularization_Experiment.py -env mountain_car -small_network -run_number $RUN1  \
- -buffer_size $BUFFER -tnet_update_freq $FREQ-lr $LR -reg_factor $RF
+ -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -reg_factor $RF
 python3 ./Regularization_Experiment.py -env mountain_car -small_network -run_number $RUN2 \
- -buffer_size $BUFFER -tnet_update_freq $FREQ-lr $LR -reg_factor $RF
+ -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -reg_factor $RF
 python3 ./Regularization_Experiment.py -env mountain_car -small_network -run_number $RUN3 \
- -buffer_size $BUFFER -tnet_update_freq $FREQ-lr $LR -reg_factor $RF
+ -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -reg_factor $RF
 python3 ./Regularization_Experiment.py -env mountain_car -small_network -run_number $RUN4 \
- -buffer_size $BUFFER -tnet_update_freq $FREQ-lr $LR -reg_factor $RF
+ -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -reg_factor $RF
 python3 ./Regularization_Experiment.py -env mountain_car -small_network -run_number $RUN5 \
- -buffer_size $BUFFER -tnet_update_freq $FREQ-lr $LR -reg_factor $RF
+ -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -reg_factor $RF
 deactivate
 
 # Parameter Sweep:
