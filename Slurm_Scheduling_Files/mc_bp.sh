661db5c3be32bb9b8f8912631814defa400fc440
jfernando <jfhernan@ualberta.ca>
2019-05-15 10:23:27 -0600

    changed to 5 runs per array job

---
+++ Slurm_Scheduling_Files/mc_bp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#SBATCH --mail-user=jfhernan@ualberta.ca
+#SBATCH --mail-type=END
+#SBATCH --array=1-100
+#SBATCH --time=10:00:00
+#SBATCH --account=def-sutton
+#SBATCH --mem-per-cpu=500M
+#SBATCH --job-name=mc_bp
+#SBATCH --output=./outputs/mc_bp-%A_%a.out
+
+source ./bin/activate
+export PYTHONPATH=.
+
+RUN1=$(($SLURM_ARRAY_TASK_ID*5 - 4))
+RUN2=$(($SLURM_ARRAY_TASK_ID*5 - 3))
+RUN3=$(($SLURM_ARRAY_TASK_ID*5 - 2))
+RUN4=$(($SLURM_ARRAY_TASK_ID*5 - 1))
+RUN5=$(($SLURM_ARRAY_TASK_ID*5))
+
+python3 ./BestParameters_Experiment.py -v -env mountain_car -buffer_size $BUFFER -m $METHOD -run_number $RUN1
+python3 ./BestParameters_Experiment.py -v -env mountain_car -buffer_size $BUFFER -m $METHOD -run_number $RUN2
+python3 ./BestParameters_Experiment.py -v -env mountain_car -buffer_size $BUFFER -m $METHOD -run_number $RUN3
+python3 ./BestParameters_Experiment.py -v -env mountain_car -buffer_size $BUFFER -m $METHOD -run_number $RUN4
+python3 ./BestParameters_Experiment.py -v -env mountain_car -buffer_size $BUFFER -m $METHOD -run_number $RUN5
+
+deactivate
