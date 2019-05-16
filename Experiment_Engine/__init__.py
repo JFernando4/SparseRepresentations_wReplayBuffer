86c507ef57cf6b4c82feaff4eed4a8033724ec1c
jfernando <jfhernan@ualberta.ca>
2019-05-16 05:28:11 -0600

    now import torch before anything else

--- Experiment_Engine/__init__.py
+++ Experiment_Engine/__init__.py
@@ -1,6 +1,8 @@
+import torch
 from .Environments import *
 from .agent import Agent
 from .function_approximators import *
 from .util import *
 from .network_evaluation import *
 from .summaries_and_statistics import *
+
