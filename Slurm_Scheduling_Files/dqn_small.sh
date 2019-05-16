4a0782ce9df649a1ffd86385b0e218625ceb510c
jfernando <jfhernan@ualberta.ca>
2019-05-16 12:46:21 -0600

    changed the name of the file and now its possible to specify env

---
+++ Slurm_Scheduling_Files/dqn_small.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#SBATCH --mail-user=jfhernan@ualberta.ca
+#SBATCH --mail-type=END
+#SBATCH --array=1-6
+#SBATCH --time=8:00:00
+#SBATCH --account=def-sutton
+#SBATCH --mem=500M
+#SBATCH --job-name=dqn_small
+#SBATCH --output=./outputs/dqn_small-%A_%a.out
+
+source ./bin/activate
+export PYTHONPATH=.
+
+# TR stands for Total Runs
+for ((i=1; i<=$TR; i++))
+do
+    RUN_NUMBER=$(($SLURM_ARRAY_TASK_ID*$TR - $TR + $i))
+    python3 ./DQN_Experiment.py -env $ENV -small_network -run_number $RUN_NUMBER \
+     -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR
+done
+
+deactivate
