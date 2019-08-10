eb4dacf4ec42fefa9aa9304f5b06a86e9c13c846
jfernando <jfhernan@ualberta.ca>
2019-08-10 15:07:36 -0600

    adapted to new dqn experiment

--- Slurm_Scheduling_Files/mc_dqn.sh
+++ Slurm_Scheduling_Files/mc_dqn.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 #SBATCH --mail-user=jfhernan@ualberta.ca
-#SBATCH --mail-type=ALL
-#SBATCH --array=1-30
+#SBATCH --mail-type=END
+#SBATCH --array=1-6
 #SBATCH --time=8:00:00
 #SBATCH --account=def-sutton
-#SBATCH --mem=1000M
-#SBATCH --job-name=mc_er
-#SBATCH --output=./outputs/mc_er-%A_%a.out
+#SBATCH --mem=500M
+#SBATCH --job-name=dqn_mc
+#SBATCH --output=./outputs/dqn_mc-%A_%a.out
 
 source ./bin/activate
 export PYTHONPATH=.
-python3 ./DQN_Experiment.py -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -env mountain_car -run_number $SLURM_ARRAY_TASK_ID
+
+# TR stands for Total Runs
+for ((i=1; i<=$TR; i++))
+do
+    RUN_NUMBER=$(($SLURM_ARRAY_TASK_ID*$TR - $TR + $i))
+    python3 ./DQN_Experiment.py -env $ENV -run_number $RUN_NUMBER \
+     -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR -v
+done
+
 deactivate
