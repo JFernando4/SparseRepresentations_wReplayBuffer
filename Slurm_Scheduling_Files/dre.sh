9b8a8ba4b3dfa8406fdeadebca968b45b88f8ced
jfernando <jfhernan@ualberta.ca>
2019-08-12 13:04:31 -0600

    updated file to experiment v2

--- Slurm_Scheduling_Files/dre.sh
+++ Slurm_Scheduling_Files/dre.sh
@@ -2,11 +2,11 @@
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=END
 #SBATCH --array=1-6
-#SBATCH --time=8:00:00
+#SBATCH --time=5:00:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=500M
-#SBATCH --job-name=dqn_small
-#SBATCH --output=./outputs/dqn_small-%A_%a.out
+#SBATCH --job-name=dre
+#SBATCH --output=./outputs/dre-%A_%a.out
 
 source ./bin/activate
 export PYTHONPATH=.
@@ -15,14 +15,13 @@ export PYTHONPATH=.
 for ((i=1; i<=$TR; i++))
 do
     RUN_NUMBER=$(($SLURM_ARRAY_TASK_ID*$TR - $TR + $i))
-    python3 ./DQN_Experiment.py -env $ENV -small_network -run_number $RUN_NUMBER \
-     -buffer_size $BUFFER -tnet_update_freq $FREQ -lr $LR
+    python3 ./DistritbutionalReg_Experiment.py -env $ENV -run_number $RUN_NUMBER -buffer_size $BUFFER -tnet_update_freq $FREQ \
+     -lr $LR -beta $BETA -reg_factor $RF  -v
 done
 
 deactivate
-
 # Parameter Sweep:
 # learning rate = {0.01, 0.004, 0.001, 0.00025} for mountain car
 # learning rate = {0.001, 0.00025, 0.0000625, 0.000015625} for catcher
-# buffer size = {100, 1k, 5k, 20k, 80k}
-# target network update frequency = {10, 50, 100, 200, 400}
+# reg_factor = {0.1, 0.01, 0.001}
+# beta = {0.1, 0.2, 0.5}
